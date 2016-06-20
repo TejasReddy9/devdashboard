2b527c8c4bf5c2901e9bf02bf35c24e3080e377d
aseemraj <aseemraj@protonmail.com>
2016-06-20 22:28:45 +0530

    Update dashboard at Mon Jun 20 22:28:45 IST 2016

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var data = {"labels": ["13 June", "14 June", "15 June", "16 June", "17 June", "18 June", "19 June"], "datasets": [{"languages": [[], [], [], [], [], ["HTML", "Rust"], ["Rust", "HTML", "JSON"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["0 secs", "0 secs", "0 secs", "0 secs", "0 secs", "14 mins", "5 hrs 16 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.0, 0.0, 0.0, 0.0, 0.0, 0.25083333333333335, 5.279166666666667], "label": "Dataset"}]};
+var totalTime = "5 hours 31 minutes";
+var languages = ["HTML", "Rust", "JSON"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
